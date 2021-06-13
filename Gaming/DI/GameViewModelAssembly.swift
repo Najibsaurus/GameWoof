a98b13027a6de040655eda5dc4f45286133abc57
Najib <inceptionslap@gmail.com>
2021-06-14 00:11:14 +0700

    add swinject library

---
+++ Gaming/DI/GameViewModelAssembly.swift
@@ -0,0 +1,18 @@
+//
+//  GameViewModelAssembly.swift
+//  Gaming
+//
+//  Created by Najib Abdillah on 14/06/21.
+//  Copyright © 2021 Najib. All rights reserved.
+//
+
+import RealmSwift
+import Swinject
+
+
+class GameViewModelAssembly: Assembly {
+    func assemble(container: Container) {
+        
+    }
+    
+}
